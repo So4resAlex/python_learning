926f37b454631539fe94acb8d3636e00fb76fa8f
Alex Soares <alealvessoares28@gmail.com>
2023-10-06 07:27:01 -0300

    Refatorada arquitetura do repo

--- tabuada.py
+++ tabuada.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
-"""Imprime a tabiado do 1 ao 10
+"""Imprime a tabiada do 1 ao 10.
 
 ---Tabuada do 1---
-    1 x 1 = 1
-    2 x 1 = 2
-    3 x 1 = 3
+    1 x 1
+    2 x 2
+    3 x 3
 ...
-##################
-Tabuada do 2
-    2 x 1 = 2
-    2 x 2 = 4
-    2 x 3 = 6
+--------------
+---Tabuada do 2---
+    2 x 1
+    4 x 2
 ...
-##################
+--------------
 """
+__version__ = "0.1.0"
+__author__ = "Alex Soares"
 
-__version__ = "0.1.1"
-__author__ = "Alex"
-
-
-numeros = list(range(1,11))
-
+numeros = list(range (1,11))
 for n1 in numeros:
     print("{:-^18}".format(f"Tabuada do {n1}"))
     for n2 in numeros:
         resultado = n1 * n2
-        print("{:^18}".format(f"{n1} x {n2} = {resultado}\n"))
-    print("#" * 18)
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print ("#" * 18)
